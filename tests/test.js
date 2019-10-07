5d92c266dc455c7be307e1937d115c673b373e3a
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-10-07 13:05:32 +0530

    Add negative test cases

--- tests/test.js
+++ tests/test.js
@@ -15,12 +15,39 @@ const TEST_CASES = [
   }
 ];
 
+const INVALID_TEST_CASES = [
+  {
+    name: 'should display contents of .travis.yml file',
+    command: 'ca .travis.yml',
+    expectedOutput: '/bin/sh: ca: command not found\n'
+  },
+  {
+    name: 'should go to tests directory and list all files',
+    command: 'cm tests && lsy',
+    expectedOutput: '/bin/sh: cm: command not found\n'
+  }
+];
+
 describe('Validates executed commands', () => {
   for (let testCase of TEST_CASES) {
     if (testCase.expectedOutput) {
       it(testCase.name, async () => {
         const actualResult = await $cExec(testCase.command);
         expect(actualResult).to.equal(testCase.expectedOutput);
+      });
+    }
+  }
+});
+
+describe('Validates failed executed commands', () => {
+  for (let testCase of INVALID_TEST_CASES) {
+    if (testCase.expectedOutput) {
+      it(testCase.name, async () => {
+        try {
+          await $cExec(testCase.command);
+        } catch (err) {
+          expect(err).to.equal(testCase.expectedOutput);
+        }
       });
     }
   }
